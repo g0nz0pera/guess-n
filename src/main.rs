ac7490ac25895c387f0c4272d6136372485fd22e
gonz0pera <155404796+g0nz0pera@users.noreply.github.com>
2024-01-23 19:06:46 -0300

    Adding the random number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::cmp::Ordering;
 use std::io;
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -11,5 +17,11 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal => println!("You Win!"),
+    }
 
 }
